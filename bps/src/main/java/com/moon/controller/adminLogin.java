50113517eac35ea7e4f15f4623c246ecd1bd0a17
SMHRD <SMHRD@121.179.7.22>
2023-09-13 16:57:24 +0900

    비밀번호 일치유무 커밋

--- bps/src/main/java/com/moon/controller/adminLogin.java
+++ bps/src/main/java/com/moon/controller/adminLogin.java
@@ -9,77 +9,68 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.moon.model.UserInfoDAO;
 import com.moon.model.compInfo;
 import com.moon.model.login;
 import com.moon.model.userInfo;
 
-
-
 @WebServlet("/Login")
 public class adminLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		
-	request.setCharacterEncoding("utf-8");
-		
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
-		
+
 		// 기능처리
 		UserInfoDAO dao = new UserInfoDAO();
-		
+
 		login user = new login();
-		
+
 		user.setEmail(id);
-		user.setPw(pw);
-		
-		login userAll = dao.info_data(user);
-		
-		
+
 	
-		
-		
-		// view 처리
-		// 실패 - LoginFail.jsp
-		
+
+		login userAll = dao.info_data(user);
 		String url = "";
 		
-		if(userAll != null) {
-			// 성공 - LoginSuccess.jsp
-			// ~~님 환영합니다 
-			// 페이지 이동시 name값 공유
+		if (userAll == null) {
+			url = "goLogin";
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			writer.println("<script>alert('존재하지않는아이디입니다.'); location.href='" + url + "';</script>");
+			writer.close();
+		}
+
+		if (!userAll.getPw().equals(pw)) {
+			url = "goLogin";
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			writer.println("<script>alert('비밀번호가 틀렸습니다.'); location.href='" + url + "';</script>");
+			writer.close();
+		} else {
+
 			String name = userAll.getName();
 			String admin = userAll.getAdmin();
 			String logo = userAll.getLogo();
 			String compName = userAll.getComp_name();
-			
+
 			request.setAttribute("name", name);
 			request.setAttribute("admin", admin);
 			request.setAttribute("logo", logo);
 			request.setAttribute("compName", compName);
 			url = "WEB-INF/Main.jsp";
-			
-		}else if (userAll == null){
-			url = "goLogin";
-			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter writer = response.getWriter();
-			writer.println("<script>alert('로그인실패'); location.href='"+url+"';</script>"); 
-			writer.close();
+
 		}
-		
-		RequestDispatcher rd =  request.getRequestDispatcher(url);
+		RequestDispatcher rd = request.getRequestDispatcher(url);
 		rd.forward(request, response);
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
